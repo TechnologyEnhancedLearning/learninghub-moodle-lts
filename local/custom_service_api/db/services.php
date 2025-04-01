a2ee4573a00cd170b1cc0296763ebe424b5c3986
Sarathlal Sarangadharan <Sarathlal.Sarangadharan@hee.nhs.uk>
2025-04-01 16:10:12 +0100

    TD-5136: A custom plugin has been created to add scorm resource programatically under a specific course

---
+++ local/custom_service_api/db/services.php
@@ -0,0 +1,35 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+$functions = array(
+   
+    'mod_scorm_insert_scorm_resource' => array(
+        'classname' => 'insert_scorm_resource',
+        'methodname' => 'insert_scorm_resource',
+        'classpath' => 'local/custom_service_api/externallib.php',
+        'description' => 'Create a scorm resource under a course',
+        'type' => 'write',
+        'ajax' => true,
+    ),
+    
+);
+
+$services = array(
+    'Insert Scorm resource' => array(
+        'functions' => array(
+           
+            'mod_scorm_insert_scorm_resource'
+        ),
+        'restrictedusers' => 0,
+        'enabled' => 1,
+          // This field os optional, but requried if the `restrictedusers` value is
+        // set, so as to allow configuration via the Web UI.
+        'shortname' =>  'InsertScorm',
+
+        // Whether to allow file downloads.
+        'downloadfiles' => 0,
+
+        // Whether to allow file uploads.
+        'uploadfiles'  => 0,
+    )
+);
