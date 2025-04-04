157a241c01025f732aa9549df98c520717117379
Sarathlal Sarangadharan <sarathlal.sarangadharan@nhs.net>
2025-04-04 08:57:44 +0100

    TD-5136:Unit test issue fix

---
+++ local/custom_service/classes/privacy/provider.php
@@ -0,0 +1,20 @@
+<?php
+namespace custom_service\privacy;
+ 
+defined('MOODLE_INTERNAL') || die;
+ 
+use core_privacy\local\metadata\provider;
+use core_privacy\local\metadata\collection;
+ 
+class provider implements provider {
+    /**
+     * Returns metadata about this plugin’s data handling.
+     * Since this plugin does not store any user data, it returns an empty collection.
+     *
+     * @param collection $collection The metadata collection.
+     * @return collection The updated metadata collection.
+     */
+    public static function get_metadata(collection $collection) : collection {
+        return $collection;
+    }
+}
