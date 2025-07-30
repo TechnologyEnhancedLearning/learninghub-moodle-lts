1ce3fcfbacf672ecf36ece75833802e23899076c
Colin Beeby <colin.beeby1@nhs.net>
2025-07-30 13:30:37 +0100

    Added terraform configuration to enable public data endpoint in moodle-test

--- Terraform/test/main.tf
+++ Terraform/test/main.tf
@@ -259,7 +259,7 @@ resource "azurerm_network_security_rule" "allow_sql_public_endpoint" {
     destination_port_range     = "3342"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
-    resource_group_name        = azurerm_resource_group.learningHubMoodleRsourceGroup.name
+    resource_group_name        = azurerm_resource_group.learningHubMoodleResourceGroup.name
     network_security_group_name = azurerm_network_security_group.nsg.name
   }
 
