2a55d7b34bfc0d2f8cfe8d37c8fc5ac3c9a8f2f4
Colin Beeby <colin.beeby1@nhs.net>
2026-07-16 16:18:25 +0100

    Changed config to point to MySQL DB

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ unset($CFG);
 global $CFG;
 $CFG = new stdClass();
 
-$CFG->dbtype    = 'sqlsrv';  // mysqli
+$CFG->dbtype    = 'mysqli';  // mysqli / sqlsrv
 $CFG->dblibrary = 'native';
 $CFG->dbhost    = 'DB-HOST-NAME';
 $CFG->dbname    = 'DB-NAME';
@@ -13,9 +13,9 @@ $CFG->dbpass    = 'DB-USER-PASSWORD';
 $CFG->prefix    = 'mdl_';
 $CFG->dboptions = array (
   'dbpersist' => true,
-  'dbport' => 1433,   // 3306
+  'dbport' => 3306,   // 3306 / 1433
   'dbsocket' => false,
-  'dbcollation' => 'SQL_Latin1_General_CP1_CI_AS'  // utf8mb4_unicode_ci
+  'dbcollation' => 'utf8mb4_unicode_ci'  // utf8mb4_unicode_ci / SQL_Latin1_General_CP1_CI_AS
 );
 
 $CFG->wwwroot   = 'WWW-ROOT';
